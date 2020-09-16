b0133c3ad1e1ccdac66537e99a75d8eadd579a32
MR.ARIS <70155682+ariszywlafa1@users.noreply.github.com>
2020-09-16 19:55:46 +0700

    Update komik.php

--- komik.php
+++ komik.php
@@ -1,54 +1,165 @@
 <?php
-        // Print out main menu..
+         // ngapain tong,jangan recode
+         // NGAPAIN NANO AMJINF
+         // Print out main menu..
+  system('sleep 2');
+ system('clear');
+   echo " JANGAN RECODE [>       ]";
+  system('sleep 1');
+system('clear');
+   echo " RECODE MANDUL [>>      ]";
+  system('sleep 1');
+system('clear');
+   echo " JANGAN RECODE [>>>     ]";
+  system('sleep 1');
+system('clear');
+   echo " RECODE MANDUL [>>>>    ]";                                                      system('sleep 1');
+system('clear');
+   echo " JANGAN RECODE [>>>>>   ]";
+  system('sleep 1');
+system('clear');                                                                           echo " RECODE MANDUL [>>>>>>  ]";
+  system('sleep 1');
+system('clear');
+   echo " JANGAN RECODE [>>>>>>> ]";
+  system('sleep 1');
+system('clear');
+   echo " RECODE MANDUL [>>>>>>>>]";
+  system('sleep 1');
+system('clear');
+
     system('clear');
-    echo "_________________________\n";
-    echo "|       KOMIK DEWASA    |\n";
-    echo "|     AUTHOR :MR.ARIS   |\n";
-    echo "|  WHATSAPP:08884196160 |\n";
-    echo "______________________________________\n";
-    echo "|       RECODE IZIN KE AUTHOR        |\n";
-    echo "|    TOOLS UNTUK DOWNLOAD PDF KOMIK  |\n";
-    echo "______________________________________\n";
-    echo "|  LINK RUSAK HUBUNGI ADMIN  |\n";
-    echo "|     JENIS KOMIK MADLOKI    |\n";
-    echo "_____________________________\n";
-    echo "pilih menu judul yang ingin di download:\n\n";
-    echo "    1) CERITA CITRA EPS 1\n";
-    echo "    2) CERITA CITRA EPS 2\n";
-    echo "    3) CERITA CITRA EPS 3\n";
-    echo "    4) UPACARA SEKOLAH\n";                                                                               
-    echo "    5) INDOMARET\n";
-    echo "    6) ICE CREAM\n";
-    echo "    0) KELUAR\n";
-    echo "Pilih Nomor : ";
-    switch(trim(fgets(STDIN,256)))
+    $green  = "\e[92m";
+    $red    = "\e[91m";
+    $yellow = "\e[93m";
+    $blue   = "\e[36m";
+system('sleep 0.1');
+    echo $red."
+ _                   _ _
+| | _____  _ __ ___ (_) | __
+| |/ / _ \| '_ ` _ \| | |/ /
+|   < (_) | | | | | | |   <
+|_|\_\___/|_| |_| |_|_|_|\_versi 1.3\n";
+system('sleep 0.5');
+echo $blue." ===========================================
+                     |   AUTHOR : MR.ARIS  |\n";
+    system('sleep  0.3');
+    echo $yellow. "    ====================================================
+    |      [    BLOK 1   ]    |      [   BLOK 2  ]     |
+    |   1) CERITA CITRA EPS 1 |  8)  TANTEKU MONTOK    |
+    |   2) CERITA CITRA EPS 2 |  9)  NARUTO X SAKURA   |
+    |   3) CERITA CITRA EPS 3 |  10) CERITA MOM 2      |
+    |   4) UPACARA SEKOLAH    |  11) CERITA MOM 3      |
+    |   5) INDOMARET          |  12) NIDURIN ANAK      |
+    |   6) ICE CREAM          |  13) SASUKE X SAKURA   |
+    |   7) TEMAN SEKELAS      |  14) TSUNADE X NARUTO  |
+    ====================================================
+    |      [   BLOK 3    ]    |
+    |   15) KUDA-KUDAAN       |
+    |   16) PR                |
+    |   17) SEBUAH PERJALANAN |
+    |   18) MIHO ICHIKI       |
+    |   19) TETANGGA NAKAL    |
+    |   20) IBUKU SELINGKUH   |
+    |   21) PACARKU SANGE     |
+    ===========================
+     KETIK 99 UNTUK KELUAR SCRIPT
+     KETIK (0) ATAU ENTER  UNTUK CHAT AUTHOR\n";
+system('sleep 0.4');
+    echo $green."
+ PILIH NOMOR YG LU INGIN LU DOWNLOAD:";
+     switch(trim(fgets(STDIN)))
         {
-            case 1:
-                echo "LINK NYA NIH GAN [ https://bit.ly/3h8Worc ]";
-                system('figlet salin tuh link');
+            case 1:                                                                                                                                          echo "LINK NYA NIH GAN \n";
+system('sleep 1');
+system('xdg-open https://bit.ly/3h8Worc');
                 exit();
             case 2:
-                echo "LINK NYA NIH GAN [ https://bit.ly/3jUYXyB ]";
-                system('figlet salin tuh link');
+                echo "LINK NYA NIH GAN\n";
+system('sleep 1');
+system('xdg-open https://bit.ly/3jUYXyB');
                 exit();
             case 3:
-                echo "LINK NYA NIH GAN [ https://bit.ly/3bz5oVi ]";
-                system('figlet salin tuh link');
+                echo "LINK NYA NIH GAN \n";
+system('sleep 1');
+system('xdg-open https://bit.ly/3bz5oVi');
                 exit();
             case 4:
-                echo "LINK NYA NIH GAN [ https://bit.ly/3bzn7fk  ]";
-                system('figlet salin tuh link');
+                echo "LINK NYA NIH GAN [ https://bit.ly/3bzn7fk ]\n";
+system('sleep 1');
+                     system('xdg-open https://bit.ly/3bzn7fk');
                 exit();
             case 5:
-                echo "LINK NYA NIH GAN [ https://bit.ly/35sUL5x ]";
-                   system('figlet salin tuh link');
+                echo "LINK NYA NIH GAN \n";
+system('sleep 1');
+system('xdg-open https://bit.ly/35sUL5x');
                 exit();
-case 6:
-                echo "LINK NYA NIH GAN [ https://bit.ly/3h7y0Gf ]";
-                   system('figlet salin tuh link');
+            case 6:
+                echo "LINK NYA NIH GAN \n";
+system('sleep 1');
+system('xdg-open https://bit.ly/3h7y0Gf');
                 exit();
-            case "0":
+            case 7:
+                echo "LINK NYA NIH GAN\n";
+system('sleep 1');
+system('xdg-open https://bit.ly/35hoOgk');
                 exit();
+            case 8:
+                echo "BELUM TERSEDIA\n";
+system('sleep 1');
+exit();
+            case 9:
+                echo "BELUM TERSEDIA\n";
+                system('sleep 1');
+exit();
+            case 10:
+echo "LINK NYA NIH GAN \n";
+system('sleep 1');
+system('xdg-open https://bit.ly/32aHhch');
+                exit();
+            case 11:
+                echo "LINK NYA NIH GAN \n";
+system('sleep 1');
+system('xdg-open https://bit.ly/336C3Od');
+                exit();
+            case 12:
+                echo "BELUM TERSEDIA\n";
+                exit();
+            case 13:
+                echo "BELUM TERSEDIA\n";
+                exit();
+            case 14:
+                echo "BELUM TERSEDIA\n";
+                exit();
+case 15:
+                echo "BELUM TERSEDIA\n";
+                exit();
+            case 16:
+                echo "BELUM TERSEDIA\n";
+                exit();
+            case 17:
+                echo "BELUM TERSEDIA\n";
+                exit();
+case 18:
+                echo "BELUM TERSEDIA\n";
+                exit();
+            case 19:
+                echo "BELUM TERSEDIA\n";
+                exit();
+            case 20:
+                echo "BELUM TERSEDIA\n";
+                exit();case 21:
+                echo "BELUM TERSEDIA\n";
+                exit();
+           case 0:
+                echo "ANDA AKAN MENGHUBUNGI AUTHOR\n";
+             system('sleep 1');
+             system('xdg-open https://wa.me/628884196160');
+exit();
+
+     case 99:
+           echo "ANDA KELUAR DARI SCRIPT\n";
+           system('sleep 3');
+           exit();
         }
     // Close standard in..
     fclose(STDIN);
